e16441bc11fb57aa081bce000957c8d92b34b887
Narindra Fiderana <narindramiharisoa1@gmail.com>
2022-04-27 11:32:55 +0300

    Conditions pour nombre des points

--- main.py
+++ main.py
@@ -28,3 +28,13 @@ for i in range(0, NB_QUESTIONS):
 
 
 print(f"Votre note est : {nb_points} / {NB_QUESTIONS}")
+moyenne = int(NB_QUESTIONS/2)
+
+if nb_points == NB_QUESTIONS:
+    print("Excellent!")
+elif nb_points == 0:
+    print("Révisez vos maths!")
+elif nb_points > moyenne:
+    print("Pas mal!")
+else:
+    print("Peut mieux faire")
